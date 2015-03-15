65cc047ccb73abc311d940d26ca3055a21b876de
nadeem hussain <nadeemhshaikh@gmail.com>
2015-03-16 00:35:35 +0530

    share d wisdom

---
+++ bld/Debug/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\nadeem\Documents\Visual Studio 2013\Projects\Security\Security\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\nadeem\Documents\Visual Studio 2013\Projects\Security\Security\bld\Debug\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ bld/Ripple/Android/Debug/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.brodysoft.sqlitePlugin/www/SQLitePlugin.js",
+        "id": "com.brodysoft.sqlitePlugin.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "file": "plugins/com.jsmobile.plugins.sms/www/sms.js",
+        "id": "com.jsmobile.plugins.sms.sms",
+        "clobbers": [
+            "window.sms"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.brodysoft.sqlitePlugin": "1.0.2",
+    "com.jsmobile.plugins.sms": "0.0.1"
+}
+// BOTTOM OF METADATA
+});
